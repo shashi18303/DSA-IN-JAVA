736c08646badc00c83ee7f47ca32369c65d9d3ca
Shashikumar Ramesh Naik <shashikumarnaiks82@gmail.com>
2026-04-17 13:07:29 +0530

    Add files via upload

---
+++ Stack/src/binarynumber.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Arrays;
+
+public class binarynumber {
+	public static void main(String[] args) {
+		int arr[]= {1,0,1,1,0};
+		Arrays.sort(arr);
+		
+		for(int x:arr) {
+			System.out.println(x);
+		}
+	}
+
+}
