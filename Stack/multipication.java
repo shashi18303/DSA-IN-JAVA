904d3772a7b72827061fd0c293b515fae59eaa31
Shashikumar Ramesh Naik <shashikumarnaiks82@gmail.com>
2026-05-08 09:49:58 +0530

    Add files via upload

---
+++ Stack/multipication.java
@@ -0,0 +1,15 @@
+package A;
+
+public class multipication {
+	public static void main(String[] args) {
+		int n=10;
+		int sum=0;
+		for(int i=0;i<=n;i++) {
+			int res=i*n;
+			sum+=res;
+		}
+		System.out.println(sum);
+	}
+	
+
+}
