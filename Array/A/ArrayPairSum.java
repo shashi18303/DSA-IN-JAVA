228a504be488b3b4108ad3525429850889f920c0
Shashikumar Ramesh Naik <shashikumarnaiks82@gmail.com>
2026-05-09 10:28:48 +0530

    Add files via upload

---
+++ Array/A/ArrayPairSum.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ArrayPairSum {
+    public static void main(String[] args) {
+
+        int[] nums = {6,2,6,5,1,2};
+
+        Arrays.sort(nums);
+
+        int sum = 0;
+
+        for(int i=0;i<nums.length;i+=2){
+            sum += nums[i];
+        }
+
+        System.out.println(sum);
+    }
+}
